70a446c7b0bd8ff3a69e029edc5dfbcb24e227d1
DzungVu <vudunghn95@gmail.com>
2018-12-14 21:46:17 +0700

    Add method sayHi for Dog

--- Dog.js
+++ Dog.js
@@ -1,7 +1,17 @@
-function Dog (){
+var chalk = require('chalk');
+
+function Dog (name){
 	this.stomach = [];
+	this.name = name;
 }
 
 Dog.prototype.eat = function (cat){
 	this.stomach.push(cat);
 }
+
+Dog.prototype.sayHi = function(){
+	console.log("Hi I'm a dog and my name is "+ chalk.red(this.name));
+
+}
+
+module.exports = Dog;
